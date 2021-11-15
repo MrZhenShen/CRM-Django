faf7d53fe66d32fa0940ece47dcb0ea1b05aeb87
mrzhenshen <mrzhenshen@gmail.com>
2021-11-15 22:20:19 +0200

    init model, create superuser

--- crmProject/crmApp/admin.py
+++ crmProject/crmApp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register your models here. 
+
+# login: admin
+# password: qwerty
+
+from .models import Client, Good, Status, Project
+
+admin.site.register(Client)
+admin.site.register(Good)
+admin.site.register(Status)
+admin.site.register(Project)
--- crmProject/crmApp/models.py
+++ crmProject/crmApp/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+class Client(models.Model):
+  first_name = models.CharField(max_length=20)
+  last_name = models.CharField(max_length=20)
+  birth_date = models.DateField
+  telephone_number = models.CharField(max_length=15)
+  email = models.CharField(max_length=30)
+  password = models.CharField(max_length=50)
+
+  is_staff = models.BooleanField(default=False)
+
+  company_name = models.CharField(max_length=30)
+  company_link = models.CharField(max_length=50)
+  industry = models.CharField(max_length=20)
+  role = models.CharField(max_length=20)
+  country = models.CharField(max_length=20)
+  city = models.CharField(max_length=20)
+
+class Good(models.Model):
+  name = models.CharField(max_length=20)
+  description = models.CharField(max_length=200)
+  example_image = models.CharField(max_length=50)
+  pricing = models.CharField(max_length=20)
+
+class Status(models.Model):
+  title = models.CharField(max_length=20)
+  color = models.CharField(max_length=20)
+
+class Project(models.Model):
+  Good = models.ForeignKey(Good, on_delete=models.CASCADE, null=True)
+  Client = models.ForeignKey(Client, on_delete=models.CASCADE, null=True)
+  client_comment = models.CharField(max_length=300)
+  Status = models.ForeignKey(Status, on_delete=models.CASCADE, null=True)
