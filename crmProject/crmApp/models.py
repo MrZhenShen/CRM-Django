9d64b305ff8cb658890d6901499049b9b240f2b7
mrzhenshen <mrzhenshen@gmail.com>
2021-11-22 13:13:33 +0200

    update models

--- crmProject/crmApp/models.py
+++ crmProject/crmApp/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Client(models.Model):
   first_name = models.CharField(max_length=20)
   last_name = models.CharField(max_length=20)
-  birth_date = models.DateField
+  birth_date = models.DateField(default='2021-12-12')
   telephone_number = models.CharField(max_length=15)
   email = models.CharField(max_length=30)
   password = models.CharField(max_length=50)
@@ -13,16 +13,16 @@ class Client(models.Model):
   is_staff = models.BooleanField(default=False)
 
   company_name = models.CharField(max_length=30)
-  company_link = models.CharField(max_length=50)
+  company_link = models.CharField(max_length=50, default='-')
   industry = models.CharField(max_length=20)
-  role = models.CharField(max_length=20)
-  country = models.CharField(max_length=20)
-  city = models.CharField(max_length=20)
+  role = models.CharField(max_length=20, default='-')
+  country = models.CharField(max_length=20, default='-')
+  city = models.CharField(max_length=20, default='-')
 
 class Good(models.Model):
   name = models.CharField(max_length=20)
   description = models.CharField(max_length=200)
-  example_image = models.CharField(max_length=50)
+  example_image = models.CharField(max_length=300)
   pricing = models.CharField(max_length=20)
 
 class Status(models.Model):
