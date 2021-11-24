a0ad48d1a98636b6cc850fda9743377cd406b55a
maxloskut <lem123squizy@gmail.com>
2021-11-24 18:56:41 +0200

    trial auth

--- crmProject/crmApp/views.py
+++ crmProject/crmApp/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets
 from rest_framework import permissions
 from .serializers import UserSerializer, GroupSerializer
 
-
+ 
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
