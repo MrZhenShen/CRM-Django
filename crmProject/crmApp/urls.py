527d054dc43a4099b1069f63321239895a1f977d
mrzhenshen <mrzhenshen@gmail.com>
2021-12-28 15:00:46 +0200

    remove extra view, urls, imports

--- crmProject/crmApp/urls.py
+++ crmProject/crmApp/urls.py
@@ -3,22 +3,18 @@ from crmApp import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path("login/", views.LoginView.as_view(), name="login"),
-    path("register/", views.RegisterView.as_view(), name="register"),
+    path("login/", views.LoginView.as_view()),
+    path("register/", views.RegisterView.as_view()),
 
-    path('clients/', views.ClientList.as_view()),
     path('clients/<int:pk>/', views.ClientDetail.as_view()),
 
     path('projects/', views.ProjectList.as_view()),
-    path('projects/<int:pk>/', views.ProjectEdit.as_view()),
-    path('projects-create/', views.ProjectCreate.as_view()),
-    path('client-projects/<int:pk>/', views.ClientProjectList.as_view()),
+    path('projects-client/<int:pk>/', views.ProjectClientList.as_view()),
+    path('project-edit/<int:pk>/', views.ProjectEdit.as_view()),
+    path('project-create/', views.ProjectCreate.as_view()),
 
     path('goods/', views.GoodList.as_view()),
-    path('goods/<int:pk>/', views.GoodDetail.as_view()),
-
-    path('statuses/', views.StatusList.as_view()),
-    path('statuses/<int:pk>/', views.StatusDetail.as_view()),
+    path('statuses/', views.StatusList.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- crmProject/crmApp/views.py
+++ crmProject/crmApp/views.py
@@ -1,27 +1,15 @@
-from django.contrib.auth.models import Permission
-from django.shortcuts import render
 from django.contrib.auth import authenticate
-
-from .models import Client, Good, Status, Project
-from .serializers import ClientSerializer, StatusSerializer, ProjectSerializer, GoodSerializer, ProjectPOSTSerializer
 from django.http import Http404
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from rest_framework import status, viewsets
+from rest_framework import status
 
-# Login
+from .models import Client, Good, Status, Project
+from .serializers import ClientSerializer, StatusSerializer, ProjectSerializer, GoodSerializer, ProjectPOSTSerializer
 
-# (<Token: f32ccbd308386ffb816782ffe852f5a982b35268>, True)
-# (<Token: d7fe63dae8b18639bd656c1bec3b93b078598e60>, True)
-
-# {
-# "email": "admin@mail.com",
-# "password": "qwerty"
-# ola@mail.com
-# 1234
-# }
-
+# Auth
 class LoginView(APIView):
     permission_classes = ()
 
@@ -49,14 +37,6 @@ class RegisterView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # Client views
-class ClientList(APIView):
-    permission_classes = [IsAuthenticated & IsAdminUser]
-
-    def get(self, request, format=None):
-        clients = Client.objects.all()
-        serializer = ClientSerializer(clients, many=True)
-        return Response(serializer.data)
-
 class ClientDetail(APIView):
     permission_classes = [IsAuthenticated]
 
@@ -79,11 +59,6 @@ class ClientDetail(APIView):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk, format=None):
-        client = self.get_object(pk)
-        client.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
 # Status views
 class StatusList(APIView):
     permission_classes = ()
@@ -93,21 +68,6 @@ class StatusList(APIView):
         serializer = StatusSerializer(statuses, many=True)
         return Response(serializer.data)
 
-class StatusDetail(APIView):
-    permission_classes = ()
-
-    def get_object(self, pk):
-        try:
-            return Status.objects.get(pk=pk)
-        except Status.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        status = self.get_object(pk)
-        serializer = StatusSerializer(status)
-        return Response(serializer.data)
-
-
 # Good views
 class GoodList(APIView):
     permission_classes = ()
@@ -116,21 +76,6 @@ class GoodList(APIView):
         goods = Good.objects.all()
         serializer = GoodSerializer(goods, many=True)
         return Response(serializer.data)
-
-class GoodDetail(APIView):
-    permission_classes = ()
-
-    def get_object(self, pk):
-        try:
-            return Good.objects.get(pk=pk)
-        except Good.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        good = self.get_object(pk)
-        serializer = GoodSerializer(good)
-        return Response(serializer.data)
-
 
 # Project views
 
@@ -159,7 +104,6 @@ class ProjectEdit(APIView):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
 class ProjectCreate(APIView):
     permission_classes = [IsAuthenticated]
 
@@ -170,7 +114,7 @@ class ProjectCreate(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class ClientProjectList(APIView):
+class ProjectClientList(APIView):
     permission_classes = [IsAuthenticated]
 
     def get_object(self, pk):
@@ -184,16 +128,3 @@ class ClientProjectList(APIView):
         projects = Project.objects.filter(Client=client)
         serializer = ProjectSerializer(projects, many=True)
         return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        project = self.get_object(pk)
-        serializer = ProjectSerializer(project, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self, request, pk, format=None):
-        project = self.get_object(pk)
-        project.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
